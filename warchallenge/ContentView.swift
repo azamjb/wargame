057ec65e3357a729a05d9144c204eb11dffdcabf
Azam Jawad Butt <azamjb123@gmail.com>
2022-01-04 22:22:35 +0000

    Initial Commit

---
+++ warchallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  warchallenge
+//
+//  Created by Azam Jawad on 2022-01-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ warchallenge/warchallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  warchallengeApp.swift
+//  warchallenge
+//
+//  Created by Azam Jawad on 2022-01-04.
+//
+
+import SwiftUI
+
+@main
+struct warchallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
